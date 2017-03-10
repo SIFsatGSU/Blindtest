1f55988c5722167f2aa28b679f695dc2c008a9cc
ZenOokami <zaneblalock@gmail.com>
2017-03-09 21:07:05 -0500

    Update with Scripts, added collider to object, mouse over for the console print.

---
+++ Unity Files/VR For the Blind/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    public float xSensitivity;
+    public float ySensitivity;
+    public float xAngleRange;
+    private float xAngle;
+    private float yAngle;
+
+    // Use this for initialization
+    void Start () {
+        xAngle = transform.eulerAngles.x;
+        yAngle = transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        xAngle -= Input.GetAxisRaw("Mouse Y") * xSensitivity;
+        yAngle += Input.GetAxisRaw("Mouse X") * ySensitivity;
+        xAngle = Mathf.Clamp(xAngle, -xAngleRange / 2, xAngleRange / 2);
+        yAngle = (yAngle % 360 + 360) % 360; // To make it cyclic. It'll always in range of 0 to 360.
+        transform.eulerAngles = new Vector3(xAngle, yAngle, 0f);
+    }
+}
---
+++ Unity Files/VR For the Blind/Assets/Scripts/RayCast2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast2 : MonoBehaviour {
+    Ray ray;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("RayCast2 Loaded Properly.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            //print(hit.collider.name);
+            if (hit.collider.gameObject.tag.Equals("ray_object"))
+            {
+                
+                print(hit.collider.name);
+            }
+        }
+		
+	}
+}
