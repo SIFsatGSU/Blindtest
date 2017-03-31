d7036f4c86415a672b0dd9bf418232e9a72a5cb5
Zane Blalock <ZaneBlalock@EssenceOfZen.org>
2017-03-31 12:32:03 -0400

    Updating the .gitignore, added Oculus Platform SDK, added Jack's tweaks to the raycast script, added sound trigger.

--- Unity Files/VR For the Blind/Assets/Scripts/HandController.cs
+++ Unity Files/VR For the Blind/Assets/Scripts/HandController.cs
@@ -13,6 +13,6 @@ public class HandController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.localPo
+        //
 	}
 }
--- Unity Files/VR For the Blind/Assets/Scripts/RayCast2.cs
+++ Unity Files/VR For the Blind/Assets/Scripts/RayCast2.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RayCast2 : MonoBehaviour {
     Ray ray;
     RaycastHit hit;
-
+    private bool alreadyHit = false;
 	// Use this for initialization
 	void Start () {
         Debug.Log("RayCast2 Loaded Properly.");
@@ -14,16 +15,40 @@ public class RayCast2 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        //Testing -- Jack's Implementation of Zane's format
+        bool hitOrNot = Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag.Equals("ray_object"); 
 
-        if(Physics.Raycast(ray, out hit))
+        if (hitOrNot && !alreadyHit) { // If mouse hits object
+            print(hit.collider.name);
+            AudioSource tone = gameObject.GetComponent<AudioSource>();
+
+            tone.Play();
+
+
+
+
+            
+        }
+
+        if (!hitOrNot && alreadyHit) { // After object is hit, and leaves 
+            print("Hit no more");
+        }
+
+        alreadyHit = hitOrNot; // Always lag behind a frame. 
+
+
+        /* Old Cold -- Deprecated 
+        if (Physics.Raycast(ray, out hit))
         {
             //print(hit.collider.name);
-            if (hit.collider.gameObject.tag.Equals("ray_object"))
+
+            /*if (hit.collider.gameObject.tag.Equals("ray_object"))
             {
-                
                 print(hit.collider.name);
             }
         }
-		
-	}
+        */
+
+    }
 }
