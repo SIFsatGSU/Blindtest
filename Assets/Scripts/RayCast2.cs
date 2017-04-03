9d586321c06a643ea48df63ca99620b51b8bef67
jackle1127 <nanotek2711@yahoo.com>
2017-04-03 16:06:14 -0400

    update with changing sounds

--- Assets/Scripts/RayCast2.cs
+++ Assets/Scripts/RayCast2.cs
@@ -7,10 +7,15 @@ public class RayCast2 : MonoBehaviour {
     Ray ray;
     RaycastHit hit;
 	public AudioSource tone;
+
+	// distance constraints
 	public float max_distance;
 	public float min_distance;
+
+	// pitch constraints 
 	public float max_pitch;
 	public float min_pitch;
+
 	public GameObject testSphere;
     private bool alreadyHit = false;
 
@@ -23,12 +28,16 @@ public class RayCast2 : MonoBehaviour {
 	void Update () {
         //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		ray = new Ray(this.transform.position, this.transform.forward);
-		Debug.DrawRay(ray.origin, ray.direction * 10);
+		Debug.DrawRay(ray.origin, ray.direction * 10); // Draws the ray on the "box" hands in the monitor window
 
         //Testing -- Jack's Implementation of Zane's format
         bool hitOrNot = Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag.Equals("ray_object"); 
-		if (hit.collider != null) {
+
+		// This simply controls the pitch levels - it does not trigger any functions however.
+		if (hit.collider != null) { // if hit.collider is touching anything.
+			// Test sphere is the sphere object that 
 			testSphere.transform.position = transform.position + hit.distance * transform.forward;
+
 			//print ((hit.collider.transform.position - transform.position).magnitude);
 			float distance = Mathf.Clamp (hit.distance, min_distance, max_distance);
 			float alpha = (distance - min_distance) / (max_distance - min_distance);
@@ -39,15 +48,18 @@ public class RayCast2 : MonoBehaviour {
 			tone.pitch = pitch;
 			print ("Pitch: " + pitch);
 		}
+
         if (hitOrNot && !alreadyHit) { // If mouse hits object
             //print(hit.collider.name);
+			tone.clip = (AudioClip) Resources.Load("Jump");
             tone.Play();
-
-            
+			Handheld.Vibrate();
         }
 
         if (!hitOrNot && alreadyHit) { // After object is hit, and leaves 
             //print("Hit no more");
+			tone.clip = (AudioClip) Resources.Load("short-bong");
+			tone.Play ();
         }
 
         alreadyHit = hitOrNot; // Always lag behind a frame. 
